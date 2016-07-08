e27a3a7e5a25347e9b0dc4856b5614d0b5eb43ea
Ajay Barot-BTC <ajay.barot@botreeconsulting.com>
2016-07-09 01:20:51 +0530

    add seed data and removed migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "admin", user_type: 2, email: "project_manager@gmail.com", password: "password", password_confirmation: "password")
+["Hire Influence", "Velocity", "Inspect Date", "Senseihub"].each do |project_name|
+	Project.create(name: project_name)
+end
+["Hiren", "Ajay", "Hardik", "Bhavi"].each do |username|
+    User.create(email: "#{username.downcase}@gmail.com", password: "password", password_confirmation: "password", username: username)
+end
